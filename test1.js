a58a377c705435b0dd1f8bdb046718fedc4febb0
Kalyan <kalyan@kalyans-MacBook-Air.local>
2020-03-19 01:00:19 +0530

    my_first_commit

---
+++ test1.js
@@ -0,0 +1,3 @@
+var name = "jagadish"
+
+module.exports = name;
---
+++ test2.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var naem1 = require('./test1')
+var app = express();
+
+var port = process.env.PORT || 3000;
+app.use('/hello' , (req,res)=> {
+    res.send(naem1)
+})
+
+app.listen(port, ()=> {console.log("server is up and running")})
